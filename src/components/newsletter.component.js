fcb984263a4612b7253cd2d9383dbc672f2d599d
Saurabh Gupta <saurabh.g@moengage.com>
2024-05-02 23:01:45 +0530

    emailjs integrated

--- src/components/newsletter.component.js
+++ src/components/newsletter.component.js
@@ -1,17 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
 export default function Newsletter() {
     const [email, setEmail] = useState('');
+    const myForm = useRef();
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        alert(email);
+    const handleSendEmail = (e) => {
+        e.preventDefault();
+        
+        emailjs
+          .sendForm('service_jszoi0o', 'template_z98docg', myForm?.current, {
+            publicKey: 'LWVU0dXbhuToT4nps',
+          })
+          .then(
+            () => {
+              console.log('SUCCESS!');
+            },
+            (error) => {
+              console.log('FAILED...', error.text);
+            },
+        );
     }
 
     return (
-        <div style={{ backgroundColor: '#1F2125', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-            <form onSubmit={handleSubmit}>
-                <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder='email address' />
+        <div className='newsletter-container'>
+            <form onSubmit={handleSendEmail} ref={myForm}>
+                <input type="email" name='user_email' value={email} onChange={(e) => setEmail(e.target.value)} placeholder='email address' />
                 <button type='submit' className='btn'>Contact</button>
             </form>
         </div>
